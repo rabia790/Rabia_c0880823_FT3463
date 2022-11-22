2422288dfd09c0bec26706e0f57eded7630f40df
Rabia790 <rabiaangel9921@gmail.com>
2022-11-21 21:33:12 -0500

    added findboxid

--- src/Move.java
+++ src/Move.java
@@ -45,4 +45,28 @@ public class Move {
         enteredString = myObj.nextLine();
         findBoxId();
     }
+
+
+    public void findBoxId() {
+        for (int i = 0; i < moveModelClasses.size(); i++) {
+            for (int count = 0; count < moveModelClasses.get(i).getCardBoardArrayList().size(); count++) {
+                for (int idCounter = 0; idCounter < moveModelClasses.get(i).getCardBoardArrayList().get(count).getObjectListArrayList().size(); idCounter++) {
+                    if (moveModelClasses.get(i).getCardBoardArrayList().get(count).getObjectListArrayList().get(idCounter).getNameOfObject().equalsIgnoreCase(enteredString)) {
+                        System.out.println(enteredString + " is in the cardboard number " + moveModelClasses.get(i).getCardBoardArrayList().get(count).getId());
+                        return;
+                    } else {
+                        System.out.println(enteredString+" object not found in cupboard "+moveModelClasses.get(i).getCardBoardArrayList().get(count).getId());
+
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    public static void main(String p[]) {
+        Move move = new Move();
+        move.initValues();
+    }
 }
