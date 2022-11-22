c39f4d46bafc02b6e0292859efc928ba6b062375
Rabia790 <rabiaangel9921@gmail.com>
2022-11-21 21:13:37 -0500

    Added move class

---
+++ src/Moving.java
@@ -0,0 +1,62 @@
+package com.example.mydummy.programs;
+
+import java.util.ArrayList;
+
+public class MoveModelClass {
+    private ArrayList<CardBoard> cardBoardArrayList;
+
+    public MoveModelClass(ArrayList<CardBoard> cardBoardArrayList) {
+        this.cardBoardArrayList = cardBoardArrayList;
+    }
+
+    public ArrayList<CardBoard> getCardBoardArrayList() {
+        return cardBoardArrayList;
+    }
+
+    public void setCardBoardArrayList(ArrayList<CardBoard> cardBoardArrayList) {
+        this.cardBoardArrayList = cardBoardArrayList;
+    }
+
+    public static class CardBoard {
+        private int id = 0;
+        private ArrayList<ObjectList>objectListArrayList;
+
+        public CardBoard(int id, ArrayList<ObjectList> objectListArrayList) {
+            this.id = id;
+            this.objectListArrayList = objectListArrayList;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public ArrayList<ObjectList> getObjectListArrayList() {
+            return objectListArrayList;
+        }
+
+        public void setObjectListArrayList(ArrayList<ObjectList> objectListArrayList) {
+            this.objectListArrayList = objectListArrayList;
+        }
+    }
+    static class ObjectList{
+        private String nameOfObject;
+
+        public ObjectList(String nameOfObject) {
+            this.nameOfObject = nameOfObject;
+        }
+
+        public String getNameOfObject() {
+            return nameOfObject;
+        }
+
+        public void setNameOfObject(String nameOfObject) {
+            this.nameOfObject = nameOfObject;
+        }
+    }
+}
+
+
